c4b2a344029867d2ad471f25c15830395bead887
Elena Bechmanis <ebechmanis@myseneca.ca>
2022-11-10 11:33:01 -0500

    added favourites component

--- components/ArtworkCardDetail.js
+++ components/ArtworkCardDetail.js
@@ -6,6 +6,7 @@ import { favouritesAtom } from "../store";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { FaRegHeart } from "react-icons/fa";
+import { Container } from "react-bootstrap";
 
 // ArtworkCardDetail (components/ArtworkCardDetail.js)
 export default function ArtworkCardDetail({ objectID }) {
@@ -35,58 +36,60 @@ export default function ArtworkCardDetail({ objectID }) {
   else
     return (
       <>
-        <Card>
-          {data.primaryImage && (
-            <Card.Img variant="top" src={data.primaryImage} />
-          )}
-          <Card.Body>
-            <Card.Title className="text-danger">
-              {data.title ? data.title : "N/A"}
-            </Card.Title>
-            <Card.Text>
-              <strong>Date: </strong>
-              {data.objectDate ? data.objectDate : "N/A"}
-              <br />
-              <strong>Classification: </strong>
-              {data.classification ? data.classification : "N/A"}
-              <br />
-              <strong>Medium: </strong>
-              {data.medium ? data.medium : "N/A"}
-              <br />
-              <br />
-              <strong>Artist: </strong>
-              {data.artistDisplayName ? (
-                <>
-                  {data.artistDisplayName} ({" "}
-                  <a
-                    href={data.artistWikidata_URL}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    wiki
-                  </a>{" "}
-                  )
-                </>
-              ) : (
-                "N/A"
-              )}
-              <br />
-              <strong>Credit Line: </strong>
-              {data.creditLine ? data.creditLine : "N/A"}
-              <br />
-              <strong>Dimensions: </strong>
-              {data.dimensions ? data.dimensions : "N/A"}
-              <br />
-            </Card.Text>
-            <Button
-              onClick={favouritesClicked}
-              className="mt-auto"
-              variant={showAdded ? "danger" : "outline-danger"}
-            >
-              {showAdded ? "Favourite (added)" : "Favourite"} <FaRegHeart />
-            </Button>
-          </Card.Body>
-        </Card>
+        <Container>
+          <Card>
+            {data.primaryImage && (
+              <Card.Img variant="top" src={data.primaryImage} />
+            )}
+            <Card.Body>
+              <Card.Title className="text-danger">
+                {data.title ? data.title : "N/A"}
+              </Card.Title>
+              <Card.Text>
+                <strong>Date: </strong>
+                {data.objectDate ? data.objectDate : "N/A"}
+                <br />
+                <strong>Classification: </strong>
+                {data.classification ? data.classification : "N/A"}
+                <br />
+                <strong>Medium: </strong>
+                {data.medium ? data.medium : "N/A"}
+                <br />
+                <br />
+                <strong>Artist: </strong>
+                {data.artistDisplayName ? (
+                  <>
+                    {data.artistDisplayName} ({" "}
+                    <a
+                      href={data.artistWikidata_URL}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      wiki
+                    </a>{" "}
+                    )
+                  </>
+                ) : (
+                  "N/A"
+                )}
+                <br />
+                <strong>Credit Line: </strong>
+                {data.creditLine ? data.creditLine : "N/A"}
+                <br />
+                <strong>Dimensions: </strong>
+                {data.dimensions ? data.dimensions : "N/A"}
+                <br />
+              </Card.Text>
+              <Button
+                onClick={favouritesClicked}
+                className="mt-auto"
+                variant={showAdded ? "danger" : "outline-danger"}
+              >
+                {showAdded ? "Favourite (added)" : "Favourite"} <FaRegHeart />
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </>
     );
 }
