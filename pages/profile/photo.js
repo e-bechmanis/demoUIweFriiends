34f5f13f764418df8df1e685ed10e5b87a52f7bc
Elena Bechmanis <ebechmanis@myseneca.ca>
2022-12-30 21:02:54 -0500

    fixed bug with photo upload UI

--- pages/profile/photo.js
+++ pages/profile/photo.js
@@ -1,33 +1,26 @@
 import { Button, Container, Form } from "react-bootstrap";
 import { useState } from "react";
+import { uploadToServer } from "../../lib/userData";
+import { useRouter } from "next/router";
 
 export default function ProfilePhoto() {
   const [image, setImage] = useState(null);
   const [objectURL, setObjectURL] = useState("");
+  const router = useRouter();
 
   const uploadPhoto = (e) => {
     if (e.target.files && e.target.files[0]) {
       const photo = e.target.files[0];
-      console.log(photo);
       setImage(photo);
-      console.log(image);
-      console.log(URL.createObjectURL(photo));
       setObjectURL(URL.createObjectURL(photo));
-      console.log(objectURL);
     }
   };
 
-  const uploadToServer = async (e) => {
-    const body = new FormData();
-    body.append("file", image);
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL_TWO}/upload`,
-      {
-        method: "POST",
-        body,
-      }
-    );
-  };
+  async function handleSubmit(e) {
+    e.preventDefault();
+    router.push(`/`);
+    await uploadToServer(image);
+  }
 
   return (
     <>
@@ -35,7 +28,7 @@ export default function ProfilePhoto() {
         <h3 className="register">Testing photo upload</h3>
         <br />
         <br />
-        <Form encType="multipart/form-data">
+        <Form encType="multipart/form-data" method="POST">
           <Form.Group>
             <Form.Label>Please upload your photo</Form.Label>
             <br />
@@ -52,9 +45,9 @@ export default function ProfilePhoto() {
             variant="outline-danger"
             className="pull-right"
             type="submit"
-            onClick={uploadToServer}
+            onClick={handleSubmit}
           >
-            Next
+            Upload
           </Button>
         </Form>
       </Container>
